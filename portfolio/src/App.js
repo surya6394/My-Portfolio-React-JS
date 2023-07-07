5d7d290dfda7c3f56c4e171ebb18f5862044507f
jyotisingh-929 <jyoti929singh@gmail.com>
2023-07-07 19:56:37 +0530

    sample commit

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,13 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import { Home } from './component/pages/Home';
+import Header from './component/pages/Header';
 
 function App() {
   return (
+    <>
+    <Header />
     <div className="App">
       <h1 style={{color: 'magenta', fontFamily: "cursive"}}>Welcome to my portfolio.</h1>
       <Home />
     </div>
+    </>
   );
 }
 
