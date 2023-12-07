721ce6df1e3c4a79e5f077af13161cb91c45ee35
surya6394 <ravinigam0720@gmail.com>
2023-12-08 00:46:22 +0530

    Todo and counter application design update

--- portfolio/src/component/HooksExample/useCallback/Callback.jsx
+++ portfolio/src/component/HooksExample/useCallback/Callback.jsx
@@ -22,7 +22,10 @@ function Callback() {
         <>
             <div className='mx-auto w-1/4 bg-slate-400 py-10 rounded-md'>
                 <Todo todos={todos} addTodo={addTodo} />
-                <div className='flex justify-center items-center'>
+                <div className='flex justify-center items-center py-3 rounded-sm shadow-[0_8px_30px_rgb(0,0,0,0.12)]'>
+                    <h1 className='font-mono font-bold text-xl bg-red-400 px-5 rounded-md text-stone-700'>Counter Operation</h1>
+                </div>
+                <div className='flex justify-center items-center pt-4'>
                     <button className='px-4 py-3 text-2xl font-bold bg-btnclr1 hover:bg-txt-clr text-txt-clr hover:text-btnclr1 rounded-2xl' onClick={Decreament}>-</button>
                     <h1 className='px-5 text-2xl font-bold'>{count}</h1>
                     <button className='px-4 py-3 text-2xl font-bold bg-btnclr1 hover:bg-txt-clr text-txt-clr hover:text-btnclr1 rounded-2xl' onClick={Increment}>+</button>
--- portfolio/src/component/HooksExample/useCallback/Todo.jsx
+++ portfolio/src/component/HooksExample/useCallback/Todo.jsx
@@ -5,10 +5,10 @@ const Todo = ({ addTodo, todos }) => {
   console.log("child render")
   return (
     <>
+      <div className='flex justify-center items-center bg-gray-300 py-3 rounded-sm shadow-[0_8px_30px_rgb(0,0,0,0.12)]'>
+        <h1 className='font-mono font-bold text-2xl text-stone-700'>Today's Task</h1>
+      </div>
       <div className='flex justify-center items-center'>
-        <div>
-          <h1>My Todo List</h1>
-        </div>
         <div>
           {todos.map((todo, index) => {
             return <p key={index}>{todo + index}</p>
@@ -16,9 +16,12 @@ const Todo = ({ addTodo, todos }) => {
         </div>
       </div>
 
-      <div className='flex justify-center'>
-        <button onClick={addTodo}>Add Todo</button>
+      <div className='flex justify-center items-center my-6'>
+        <button className='p-3 px-8 bg-red-400 text-xl font-serif font-bold rounded-xl shadow-[0_2.8px_2.2px_rgba(0,_0,_0,_0.034),_0_6.7px_5.3px_rgba(0,_0,_0,_0.048),_0_12.5px_10px_rgba(0,_0,_0,_0.06),_0_22.3px_17.9px_rgba(0,_0,_0,_0.072),_0_41.8px_33.4px_rgba(0,_0,_0,_0.086),_0_100px_80px_rgba(0,_0,_0,_0.12)]' 
+        onClick={addTodo}>Add Todo</button>
       </div>
+
+      <hr className='flex justify-center border-1 border-gray-900 mx-auto w-2/3 my-5' />
     </>
   )
 }
